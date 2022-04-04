dcae7bf99d1ba401cf9af2a2ceb21ac12f3d392d
mibao2022 <102961004+mibao2022@users.noreply.github.com>
2022-04-04 19:07:34 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,132 @@
 <?php
 
 /**
- *****************************************************************************************************
- *    如果您通过浏览器访问网站时看到了这个提示，那么我们很遗憾地通知您，您的空间不支持 PHP 。
- *    也就是说，您的空间可能是静态空间，或没有安装PHP，或没有为 Web 服务器打开 PHP 支持。
- *    Sorry, PHP is not installed on your web hosting if you see this prompt.
- *    Please check out the PHP configuration.
- *
- *    如您使用虚拟主机：
- *
- *        > 联系空间商，更换空间为支持 PHP 的空间。
- *        > Contact your service provider, and let them provice a new service which supports PHP.
- *
- *
- *    如您自行搭建服务器，推荐您：
- *    Configuring manually? Recommend:
- *
- *        > 访问 PHP 官方网站获取安装帮助。
- *        > Visit PHP Official Website to get the documentation of installion and configuration.
- *        > http://php.net
- *
- ******************************************************************************************************
- */
-
-/**
  * Z-Blog with PHP.
  *
- * @author Z-BlogPHP Team
- * @version
+ * @author  Z-BlogPHP Team
+ * @version 2.0 2013-07-05
  */
-require 'zb_system/function/c_system_base.php';
+require '../function/c_system_base.php';
+require '../function/c_system_admin.php';
 
-$zbp->RedirectInstall();
 $zbp->Load();
-$zbp->CheckSiteClosed();
 
-foreach ($GLOBALS['hooks']['Filter_Plugin_Index_Begin'] as $fpname => &$fpsignal) {
+$zbp->action = GetVars('act', 'GET');
+
+$admin_function = null;
+
+$zbp->action = ($zbp->action == '') ? 'admin' : $zbp->action;
+
+if (!$zbp->CheckRights($zbp->action)) {
+    $zbp->ShowError(6, __FILE__, __LINE__);
+    die();
+}
+
+foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_Begin'] as $fpname => &$fpsignal) {
     $fpname();
 }
 
-ViewIndex();
+switch ($zbp->action) {
+    case 'ArticleMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_ArticleMng';
+            $blogtitle = $lang['msg']['article_manage'];
+        }
+        break;
+    case 'PageMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_PageMng';
+            $blogtitle = $lang['msg']['page_manage'];
+        }
+        break;
+    case 'CategoryMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_CategoryMng';
+            $blogtitle = $lang['msg']['category_manage'];
+        }
+        break;
+    case 'CommentMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_CommentMng';
+            $blogtitle = $lang['msg']['comment_manage'];
+            if (GetVars('ischecking', 'GET') == true) {
+                $blogtitle .= ' - ' . $GLOBALS['lang']['msg']['check_comment'];
+            }
+        }
+        break;
+    case 'MemberMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_MemberMng';
+            $blogtitle = $lang['msg']['member_manage'];
+        }
+        break;
+    case 'UploadMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_UploadMng';
+            $blogtitle = $lang['msg']['upload_manage'];
+        }
+        break;
+    case 'TagMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_TagMng';
+            $blogtitle = $lang['msg']['tag_manage'];
+        }
+        break;
+    case 'PluginMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_PluginMng';
+            $blogtitle = $lang['msg']['plugin_manage'];
+        }
+        break;
+    case 'ThemeMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_ThemeMng';
+            $blogtitle = $lang['msg']['theme_manage'];
+        }
+        break;
+    case 'ModuleMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_ModuleMng';
+            $blogtitle = $lang['msg']['module_manage'];
+        }
+        break;
+    case 'SettingMng':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_SettingMng';
+            $blogtitle = $lang['msg']['settings'];
+        }
+        break;
+    case 'admin':
+        if (is_null($admin_function)) {
+            $admin_function = 'Admin_SiteInfo';
+            $blogtitle = $lang['msg']['dashboard'];
+        }
+        break;
+    default:
+        foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_Other_Action'] as $fpname => &$fpsignal) {
+            $fpsignal = $fpname($zbp->action, $admin_function);
+            if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN || $fpsignal == PLUGIN_EXITSIGNAL_BREAK) {
+                break;
+            }
+        }
+        unset($fpsignal);
+        break;
+}
 
-foreach ($GLOBALS['hooks']['Filter_Plugin_Index_End'] as $fpname => &$fpsignal) {
+require ZBP_PATH . 'zb_system/admin/admin_header.php';
+require ZBP_PATH . 'zb_system/admin/admin_top.php';
+
+?>
+<div id="divMain">
+    <?php
+    $admin_function();
+    ?>
+</div>
+<?php
+require ZBP_PATH . 'zb_system/admin/admin_footer.php';
+
+foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_End'] as $fpname => &$fpsignal) {
     $fpname();
 }
+
+RunTime();
